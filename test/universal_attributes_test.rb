5cec993eaa93571813aefc6db49d77c517de8082
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-03-31 15:53:51 -0600

    Expand a couple of tests

--- test/universal_attributes_test.rb
+++ test/universal_attributes_test.rb
@@ -6,7 +6,7 @@ class UniversalAttributesTest < ActiveSupport::TestCase
   setup do
     hash = {id: 1, test: true, example: "value", other: nil, created_at: Time.current, updated_at: Time.current}
     @universal_attributes = UniversalID::Attributes.new(hash)
-    @gid_options = {app: "test-gid", verifier: GlobalID::Verifier.new("secret")}.freeze
+    @gid_options = {app: "test-gid", verifier: GlobalID::Verifier.new("secret")}
   end
 
   def test_to_gid_app
@@ -15,6 +15,8 @@ class UniversalAttributesTest < ActiveSupport::TestCase
 
   def test_to_gid
     gid = @universal_attributes.to_gid(@gid_options)
+    assert_equal "gid://test-gid/UniversalID::Attributes/eNqrVipJLS5RsiopKk3VUUqtSMwtyElVslIqS8wpTVWqBQCv9wru", gid.to_s
+    assert_equal "Z2lkOi8vdGVzdC1naWQvVW5pdmVyc2FsSUQ6OkF0dHJpYnV0ZXMvZU5xclZpcEpMUzVSc2lvcEtrM1ZVVXF0U013dHlFbFZzbElxUzh3cFRWV3FCUUN2OXdydQ", gid.to_param
     assert_equal({"test" => true, "example" => "value"}, gid.find)
   end
 
@@ -24,6 +26,9 @@ class UniversalAttributesTest < ActiveSupport::TestCase
 
   def test_to_sgid
     sgid = @universal_attributes.to_sgid(@gid_options)
+    expected = "BAh7CEkiCGdpZAY6BkVUSSJgZ2lkOi8vdGVzdC1naWQvVW5pdmVyc2FsSUQ6OkF0dHJpYnV0ZXMvZU5xclZpcEpMUzVSc2lvcEtrM1ZVVXF0U013dHlFbFZzbElxUzh3cFRWV3FCUUN2OXdydQY7AFRJIgxwdXJwb3NlBjsAVEkiDGRlZmF1bHQGOwBUSSIPZXhwaXJlc19hdAY7AFQw--dd496edc94ad3bf7f9604db57bbdeaf91f563d5f"
+    assert_equal expected, sgid.to_s
+    assert_equal expected, sgid.to_param
     assert_equal({"test" => true, "example" => "value"}, sgid.find)
   end
 end
