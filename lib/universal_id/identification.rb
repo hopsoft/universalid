d3686618f3a275811524537f957febbae9252adc
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-03-31 14:33:41 -0600

    Setup new shorter aliases for identification

--- lib/universal_id/identification.rb
+++ lib/universal_id/identification.rb
@@ -14,11 +14,13 @@ module UniversalID
         new GlobalID.parse(universal_attributes_gid, DEFAULT_OPTIONS.merge(options)).find
       end
       alias_method :new_from_universal_attributes_gid, :new_from_universal_attributes_global_id
+      alias_method :new_from_ugid, :new_from_universal_attributes_global_id
 
       def new_from_universal_attributes_signed_global_id(universal_attributes_sgid, options = {})
         new SignedGlobalID.parse(universal_attributes_sgid, DEFAULT_OPTIONS.merge(options)).find
       end
       alias_method :new_from_universal_attributes_sgid, :new_from_universal_attributes_signed_global_id
+      alias_method :new_from_usgid, :new_from_universal_attributes_signed_global_id
     end
 
     def universal_attributes
@@ -29,20 +31,24 @@ module UniversalID
       GlobalID.create universal_attributes, DEFAULT_OPTIONS.merge(options)
     end
     alias_method :to_universal_attributes_gid, :to_universal_attributes_global_id
+    alias_method :to_ugid, :to_universal_attributes_global_id
 
     def to_universal_attributes_global_id_param(options = {})
       to_universal_attributes_gid(options).to_param
     end
     alias_method :to_universal_attributes_gid_param, :to_universal_attributes_global_id_param
+    alias_method :to_ugid_param, :to_universal_attributes_global_id_param
 
     def to_universal_attributes_signed_global_id(options = {})
       SignedGlobalID.create universal_attributes, DEFAULT_OPTIONS.merge(options)
     end
     alias_method :to_universal_attributes_sgid, :to_universal_attributes_signed_global_id
+    alias_method :to_usgid, :to_universal_attributes_signed_global_id
 
     def to_universal_attributes_signed_global_id_param(options = {})
       to_universal_attributes_sgid(options).to_param
     end
     alias_method :to_universal_attributes_sgid_param, :to_universal_attributes_signed_global_id_param
+    alias_method :to_usgid_param, :to_universal_attributes_signed_global_id_param
   end
 end
