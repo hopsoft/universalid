f11dfede66e06bea2f6404db7e076e5195a05c8e
Julian Rubisch <julian@julianrubisch.at>
2023-10-18 11:38:40 +0200

    fix: Fall back to scanning AR descendants if model can't be found

--- lib/universal_id/relation_identification.rb
+++ lib/universal_id/relation_identification.rb
@@ -7,7 +7,7 @@ module UniversalID::RelationIdentification
     compressed_sql = Base64.urlsafe_decode64(id)
     sql = Zlib::Inflate.inflate(compressed_sql)
 
-    extract_model_name(sql).safe_constantize.find_by_sql(Arel.sql(sql))
+    extract_model_class(sql).find_by_sql(Arel.sql(sql))
   rescue => error
     raise UniversalID::LocatorError.new(id, error)
   end
@@ -19,9 +19,17 @@ module UniversalID::RelationIdentification
 
   private
 
-  def extract_model_name(sql)
-    table_name = extract_table_name(sql)
-    table_name&.classify
+  def extract_model_class(sql)
+    find_model_by_table_name(extract_table_name(sql))
+  end
+
+  def find_model_by_table_name(table_name)
+    class_name = table_name&.classify
+
+    return class_name.safe_constantize if Object.const_defined?(class_name)
+
+    # this is costly in terms of memory
+    ActiveRecord::Base.descendants.detect { |model| model.table_name == table_name }
   end
 
   def extract_table_name(sql)
