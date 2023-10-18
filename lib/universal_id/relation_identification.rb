ec9dc7b22ac254cf793da3344784cca3a13c6d6b
Julian Rubisch <julian@julianrubisch.at>
2023-10-18 11:05:53 +0200

    feat: Draft model detection

--- lib/universal_id/relation_identification.rb
+++ lib/universal_id/relation_identification.rb
@@ -5,8 +5,9 @@ module UniversalID::RelationIdentification
 
   def find(id)
     compressed_sql = Base64.urlsafe_decode64(id)
-    # TODO this doesn't re-instantiate the relation
-    ActiveRecord::Base.connection.select_all(Arel.sql(Zlib::Inflate.inflate(compressed_sql)))
+    sql = Zlib::Inflate.inflate(compressed_sql)
+
+    extract_model_name(sql).safe_constantize.find_by_sql(Arel.sql(sql))
   rescue => error
     raise UniversalID::LocatorError.new(id, error)
   end
@@ -14,5 +15,17 @@ module UniversalID::RelationIdentification
   def id
     compressed_sql = Zlib::Deflate.deflate(to_sql, Zlib::BEST_COMPRESSION)
     Base64.urlsafe_encode64 compressed_sql, padding: false
+  end
+
+  private
+
+  def extract_model_name(sql)
+    table_name = extract_table_name(sql)
+    table_name&.classify
+  end
+
+  def extract_table_name(sql)
+    match_data = sql.match(/FROM\s+"?([a-zA-Z0-9_]+)"?/)
+    match_data[1] if match_data
   end
 end
