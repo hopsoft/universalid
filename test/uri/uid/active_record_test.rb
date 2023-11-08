6fd8b4188a8fa849829f4a4babdc10668648891b
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-11-08 09:04:32 -0700

    Add URI::UID tests for ActiveRecord

---
+++ test/uri/uid/active_record_test.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+require_relative "../../test_helper"
+
+class URI::UID::ActiveRecordTest < Minitest::Test
+  def test_new_model
+    with_new_campaign do |campaign|
+      uid = URI::UID.create(campaign)
+      expected = "uid://universal-id/G0QAAIyUqtsjPVl5TlKzKZpALGZjpKCBvwzJgkOO7KD2LaDmEdXYkpn7hLkDzVNFme8I9aQWSOwRNg"
+      assert_equal expected, uid.to_s
+
+      decoded = URI::UID.parse(uid.to_s).decode
+      assert_equal campaign.class, decoded.class
+      refute_equal campaign.attributes, decoded.attributes
+    end
+  end
+
+  def test_new_model_include_unsaved_changes
+    with_new_campaign do |campaign|
+      uid = URI::UID.create(campaign, include_unsaved_changes: true)
+      decoded = URI::UID.parse(uid.to_s).decode
+      assert_equal campaign.class, decoded.class
+      assert_equal campaign.attributes, decoded.attributes
+    end
+  end
+
+  def test_new_model_include_unsaved_changes_exclude_blanks
+    with_new_campaign do |campaign|
+      uid = URI::UID.create(campaign, include_unsaved_changes: true, include_blank: false)
+      decoded = URI::UID.parse(uid.to_s).decode
+      assert_equal campaign.class, decoded.class
+      assert_equal campaign.attributes, decoded.attributes
+    end
+  end
+
+  def test_persisted_model
+    with_persisted_campaign do |campaign|
+      uid = URI::UID.create(campaign)
+      decoded = URI::UID.parse(uid.to_s).decode
+      assert_equal campaign, decoded
+    end
+  end
+
+  def test_changed_persisted_model
+    with_persisted_campaign do |campaign|
+      campaign.description = "Changed Description"
+      uid = URI::UID.create(campaign)
+      decoded = URI::UID.parse(uid.to_s).decode
+      assert_equal campaign, decoded
+      refute_equal campaign.description, decoded.description
+    end
+  end
+
+  def test_changed_persisted_model_include_unsaved_changes
+    with_persisted_campaign do |campaign|
+      campaign.description = "Changed Description"
+      uid = URI::UID.create(campaign, include_unsaved_changes: true)
+      decoded = URI::UID.parse(uid.to_s).decode
+      assert_equal campaign, decoded
+      assert_equal campaign.description, decoded.description
+    end
+  end
+
+  def test_changed_persisted_model_with_registered_custom_config
+    yaml = <<~YAML
+      prepack:
+      exclude:
+      - description
+      - trigger
+      include_blank: false
+
+      database:
+      include_unsaved_changes: true
+      include_timestamps: false
+    YAML
+
+    _, settings = UniversalID::Settings.register("test_#{SecureRandom.alphanumeric(8)}", YAML.safe_load(yaml))
+
+    with_persisted_campaign do |campaign|
+      campaign.name = "Changed Name"
+      campaign.description = "Changed Description"
+      campaign.trigger = "Changed Trigger"
+
+      uid = URI::UID.create(campaign, **settings)
+      decoded = URI::UID.parse(uid.to_s).decode
+      assert_equal campaign, decoded
+      assert_equal campaign.name, decoded.name
+      assert_nil decoded.description
+      assert_nil decoded.trigger
+    end
+  end
+end
