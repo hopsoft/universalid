b793e518da384ac85a576d561a40de72104bf748
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-11-14 20:26:39 -0700

    Exploratory work for introducing a prepack observer

--- lib/universal_id/prepacker.rb
+++ lib/universal_id/prepacker.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "observer"
 require_relative "refinements"
 
 class UniversalID::Prepacker
@@ -9,20 +10,44 @@ class UniversalID::Prepacker
   using UniversalID::Refinements::SetRefinement
   using UniversalID::Refinements::OpenStructRefinement
 
-  class CircularReferenceError < StandardError
-    def initialize(message = "Prepacking not supported on self referencing objects!")
-      super
-    end
-  end
+  extend Observable
 
   class << self
     def prepack(object, options = {})
       options = UniversalID::PrepackOptions.new(options) unless options.is_a?(UniversalID::PrepackOptions)
 
-      return object.prepack(options) if object.respond_to?(:prepack)
+      notify :before, object
 
-      object.instance_variable_set(:@_uid_prepack_options, options) unless object.frozen?
-      object
+      prepacked = if object.respond_to?(:prepack)
+        object.prepack options
+      else
+        object.instance_variable_set(:@_uid_prepack_options, options) unless object.frozen?
+        object
+      end
+
+      notify :after, object, prepacked
+
+      prepacked
+    end
+
+    def notify_object(event, object, prepacked = nil)
+      method_name = "#{event}_prepack"
+      object.public_send method_name, object, prepacked if object.respond_to?(method_name)
+    end
+
+    private
+
+    def notify(event, *args)
+      changed
+      notify_observers event, *args
+    end
+  end
+
+  add_observer self, :notify_object
+
+  class CircularReferenceError < StandardError
+    def initialize(message = "Prepacking not supported on self referencing objects!")
+      super
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,6 +12,7 @@ require "minitest/parallel"
 require "minitest/reporters"
 require "pry-byebug"
 require "pry-doc"
+require "rainbow"
 require "simplecov"
 
 # MiniTest setup
@@ -39,3 +40,27 @@ require "universal_id"
 
 # Load contribs
 require "universal_id/contrib/rails"
+
+if ENV["BENCHMARK_PREPACK"]
+  class PrepackObserver
+    class << self
+      def log_prepack_event(event, object, prepacked = nil)
+        case event
+        when :before
+          object.instance_variable_set(:@_uid_prepack_start, Time.now) unless object.frozen?
+          puts Rainbow("\n#{event} #{object.class.name}#prepack".ljust(80, "=")).cyan.bright
+          ap object
+        when :after
+          start = object.remove_instance_variable(:@_uid_prepack_start) if object.instance_variable_defined?(:@_uid_prepack_start)
+          time = (Time.now - start).real.round(5) if start
+          print Rainbow("#{event} #{object.class.name}#prepack").cyan
+          print Rainbow("(#{"%1.3f sec" % time})").cyan + Rainbow("(#{"%6.2fms" % (time * 1_000)})").cyan.bright if time
+          puts
+          ap prepacked
+        end
+      end
+    end
+  end
+
+  UniversalID::Prepacker.add_observer PrepackObserver, :log_prepack_event
+end
