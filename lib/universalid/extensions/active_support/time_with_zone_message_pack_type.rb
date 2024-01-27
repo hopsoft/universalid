38fc43c6c3d760e9e327b1cfe47aaa00c4b221cd
Nate Hopkins (hopsoft) <natehop@gmail.com>
2024-01-26 18:48:11 -0700

    Cleanup

--- lib/universalid/extensions/active_support/time_with_zone_message_pack_type.rb
+++ lib/universalid/extensions/active_support/time_with_zone_message_pack_type.rb
@@ -7,14 +7,11 @@ if defined? ActiveSupport::TimeWithZone
     packer: ->(obj, packer) do
       packer.write obj.to_time.utc
       packer.write obj.time_zone.tzinfo.identifier
-      # packer.write obj.iso8601(9)
     end,
     unpacker: ->(unpacker) do
       utc = unpacker.read
       tz = unpacker.read
       utc.in_time_zone ActiveSupport::TimeZone[tz]
-      # time = Time.parse(unpacker.read)
-      # time.in_time_zone ActiveSupport::TimeZone[time.zone]
     end
   )
 
